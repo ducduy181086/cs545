582235d5a87e23b6ed4b746f46d55f1abc16cf58
Duc Duy Nguyen <ducduy181086@gmail.com>
2024-10-30 10:07:41 -0500

    updated

--- lab01/src/main/java/edu/miu/cs545/lab01/helper/FilterParser.java
+++ lab01/src/main/java/edu/miu/cs545/lab01/helper/FilterParser.java
@@ -11,11 +11,9 @@ public class FilterParser {
             return filtersMap;
         }
 
-        // Tách các điều kiện theo dấu '|'
         String[] conditions = filter.split("\\|");
 
         for (String condition : conditions) {
-            // Tách key và value theo dấu '::'
             String[] keyValue = condition.split("::");
             if (keyValue.length == 2) {
                 String key = keyValue[0].trim();
