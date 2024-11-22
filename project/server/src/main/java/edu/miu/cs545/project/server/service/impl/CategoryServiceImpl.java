6bfdc9101f0b2b2e1ed40881c70d9a6d32909464
Duc Duy Nguyen <ducduy181086@gmail.com>
2024-11-21 21:56:41 -0600

    update categories

--- project/server/src/main/java/edu/miu/cs545/project/server/service/impl/CategoryServiceImpl.java
+++ project/server/src/main/java/edu/miu/cs545/project/server/service/impl/CategoryServiceImpl.java
@@ -13,7 +13,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 @Service
