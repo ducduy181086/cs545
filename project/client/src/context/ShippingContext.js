4358c6ac075f5749c60770cd6dd9d68e77276475
Anh Nguyễn <kzjn10@users.noreply.github.com>
2024-11-21 21:50:46 -0600

    feat: store data to context

--- project/client/src/context/ShippingContext.js
+++ project/client/src/context/ShippingContext.js
@@ -6,22 +6,26 @@ export const ShippingContext = createContext();
 
 // CartProvider component to provide context to the rest of the app
 export const ShippingProvider = ({ children }) => {
+    const key = 'shippingInfo';
     // State to hold cart items
-    const [shippingInfo, setShippingInfo] = useState(null);
+    const [shippingInfo, setShippingInfo] = useState(JSON.parse(localStorage.getItem(key)));
 
     // Function to add shipping info
     const updateShippingInfo = (shippingInfo) => {
         setShippingInfo(shippingInfo);
+        localStorage.setItem(key, JSON.stringify(shippingInfo));
     };
 
     // Function to add billing info
     const updateBillingInfo = (shippingInfo) => {
         setShippingInfo(shippingInfo);
+        localStorage.setItem(key, JSON.stringify(shippingInfo));
     };
 
     // Function to clear the shipping info
     const clearShippingInfo = () => {
         setShippingInfo(null);
+        localStorage.removeItem(key);
     };
 
     return (
