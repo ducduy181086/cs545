39f6dda7070641a4d5f1d7a4c293fe3e996829e2
Duc Duy Nguyen <ducduy181086@gmail.com>
2024-11-22 00:18:17 -0600

    update order date

--- project/server/src/main/java/edu/miu/cs545/project/server/service/impl/OrderServiceImpl.java
+++ project/server/src/main/java/edu/miu/cs545/project/server/service/impl/OrderServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -159,5 +160,7 @@ public class OrderServiceImpl implements OrderService {
         orderEntity.setPaymentType(order.getPaymentType());
         orderEntity.setPaymentDetails(order.getPaymentDetails());
         orderEntity.setPaymentStatus(order.getPaymentStatus());
+
+        orderEntity.setOrderDate(LocalDateTime.now());
     }
 }
